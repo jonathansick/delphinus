323d28b64cf884393b0d85e8b182b3aec864a4de
Jonathan Sick <jonathansick@mac.com>
2013-04-29 17:01:56 -0400

    Fix basic implemention bugs in StarList

--- delphinus/artstars.py
+++ delphinus/artstars.py
@@ -20,13 +20,13 @@ class StarList(object):
         self.nImages = nImages
         self._groups = []  # one item for each add_stars() call.
     
-    def add_stars(self, x, y, ext=0, chip=1, *counts):
+    def add_stars(self, x, y, counts, ext=0, chip=1):
         """Add a list of stars for a given `ext` and `chip`.
         
         x,y : ndarray
             Pixel coordinates of artificial stars in frame of the reference
             image (or first image).
-        *counts : ndarray
+        counts : tuple
             Artificial stars brightnesses, in counts, for each image.
             The order must be consistent with the ordering of each image
             photometered in the original DOLPHOT run.
@@ -48,7 +48,7 @@ class StarList(object):
 
         self._groups.append(stars)
 
-    def add_stars_mags(self, x, y, zps=25, exptimes=1., ext=0, chip=1, *mags):
+    def add_stars_mags(self, x, y, mags, zps=25, exptimes=1., ext=0, chip=1):
         """Identical to :func:`add_stars` except that star brightnesses can
         be given as magnitudes. Zeropoints and exposure times will translate
         those magnitudes into counts on each image.
@@ -61,20 +61,21 @@ class StarList(object):
         counts = []
         for mag, zp, exptime in zip(mags, zps, exptimes):
             counts.append(10. ** (-0.4 * (mag + zp)) * exptime)
-        self.add_stars(x, y, *counts, ext=ext, chip=chip)
+        self.add_stars(x, y, counts, ext=ext, chip=chip)
     
     def write(self, path):
         """Write articial star list to `path`."""
         # Prep output path
         dirname = os.path.dirname(path)
-        if not os.path.exists(dirname): os.makedirs(dirname)
+        if (dirname is not "") and (not os.path.exists(dirname)):
+            os.makedirs(dirname)
         if os.path.exists(path): os.remove(path)
 
         # Stack Data
         allStars = np.vstack(tuple(self._groups))
 
         # Write star list
-        fmt = ['%i', '%i', '%.3f' '%.3f']
+        fmt = ['%i', '%i', '%.3f', '%.3f']
         for i in xrange(self.nImages):
             fmt.append("%.6f")
-        np.savetxt(path, allStars, delimeter=' ', newline='\n', fmt=tuple(fmt))
+        np.savetxt(path, allStars, delimiter=' ', newline='\n', fmt=tuple(fmt))
