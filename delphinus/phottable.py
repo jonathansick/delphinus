eed7ff81c22f89915db7b8353564d5f5c8aec123
Jonathan Sick <jonathansick@mac.com>
2013-05-01 16:09:22 -0400

    Add WIRCamFakeTable to quickly parse ASTs
    WIRCamFakeTable can read the AST results from two-image WIRCam
    artificial star tests. Ultimately I want to refactor DolphotTable
    so make Fake tables and general photometry tables inherit the same
    parsing structure.

--- delphinus/phottable.py
+++ delphinus/phottable.py
@@ -144,3 +144,93 @@ class DolphotTable(object):
             plotPath = "%s_%s_%s.%s" % (plotPathRoot, imageKey, band, fmt)
             lfdiagnostics.make_diagnostic_plot(self, i, imageKey,
                     band, fmt, plotPath, magLim)
+
+
+class WIRCamFakeTable(object):
+    """Quick and dirty interface to DOLPHOT's .fake output files from
+    artificial star tests.
+    
+    .. todo:: Enable this AST photometry to be embedded in a dolphot HDF5
+       output table.
+    """
+    def __init__(self, fakePath, refImagePath=None):
+        super(WIRCamFakeTable, self).__init__()
+        self.path = fakePath
+        self._data = self._read(refImagePath)
+
+    def _read(self, refImagePath=None):
+        """Read .fake file."""
+        n = 2  # two images in WIRCam testing
+        # TODO add columns for fake stars/recovery
+        colDefs = np.dtype([
+            # Fake star columns
+            ('fake_ext', np.int), ('fake_chip', np.int),
+            ('fake_x', np.float), ('fake_y', np.float),
+            ('fake_count_1', np.float), ('fake_mag_1', np.float),
+            ('fake_count_2', np.float), ('fake_mag_2', np.float),
+            # Regular columns
+            ('ext', np.int), ('chip', np.int),
+            ('x', np.int), ('y', np.int),
+            ('ra', np.float), ('dec', np.float), ('ref_chi', np.float),
+            ('ref_sn', np.float), ('ref_sharp', np.float),
+            ('ref_round', np.float), ('major_ax', np.int),
+            ('ref_crowding', np.float), ('type', np.int),
+            ('counts', np.float, n),
+            ('sky', np.float, n), ('norm_count_rate', np.float, n),
+            ('norm_count_rate_err', np.float, n), ('mag', np.float, n),
+            ('mag_err', np.float, n), ('chi', np.float, n),
+            ('sn', np.float, n),
+            ('sharp', np.float, n), ('round', np.float, n),
+            ('crowding', np.float, n), ('fwhm', np.float, n),
+            ('ecc', np.float, n), ('psf_a', np.float, n),
+            ('psf_b', np.float, n),
+            ('psf_c', np.float, n), ('quality', np.int, n)])
+
+        # Data type for numpy load txt
+        # Prepended columns for fake stars
+        dataTypes = [np.int, np.int, np.float, np.float, # fake
+                np.float, np.float, np.float, np.float,  # fake counts/mags
+                np.int, np.int, np.int, np.int,
+                np.float, np.float, np.float, np.float, np.int,
+                np.float, np.int]
+        colNames = ['fake_ext', 'fake_chip', 'fake_x', 'fake_y',
+                'fake_count_1', 'fake_mag_1', 'fake_count_2', 'fake_mag_2',
+                'ext', 'chip', 'x', 'y', 'ref_chi', 'ref_sn',
+                'ref_sharp', 'ref_round', 'major_ax', 'ref_crowding', 'type']
+        imgDataTypes = [np.float, np.float, np.float,
+                np.float, np.float, np.float, np.float, np.float,
+                np.float, np.float,
+                np.float, np.float,
+                np.float, np.float, np.float,
+                np.float, np.int]
+        imgColNames = ['counts', 'sky', 'norm_count_rate',
+                'norm_count_rate_err',
+                'mag', 'mag_err', 'chi', 'sn', 'sharp', 'round', 'crowding',
+                'fwhm', 'ecc', 'psf_a', 'psf_b', 'psf_c', 'quality']
+        for i in xrange(n):
+            dataTypes += imgDataTypes
+        data = np.loadtxt(self.path)
+        nStars = data.shape[0]
+        dataRecArray = np.empty(nStars, dtype=colDefs)
+        # Get columns for general star data
+        for i, colName in enumerate(colNames):
+            dataRecArray[colName] = data[:, i]
+        # Get columns for measurements from specific images
+        for j in xrange(n):
+            for i, colName in enumerate(imgColNames):
+                k = j * len(imgDataTypes) + len(colNames) + i
+                dataRecArray[colName][:, j] = data[:, k]
+
+        # Compute RA and Dec from x,y coords against ref or first image
+        if refImagePath is not None:
+            refHead = getheader(refImagePath)
+            wcs = WCS(refHead)
+            ra, dec = wcs.all_pix2sky(dataRecArray['x'], dataRecArray['y'], 1)
+            dataRecArray['ra'] = ra
+            dataRecArray['dec'] = dec
+
+        return dataRecArray
+
+if __name__ == '__main__':
+    fakePath = "/Users/jsick/Dropbox/_dolphot/517eef6ce8f07284365c6156.fake"
+    fakeTable = WIRCamFakeTable(fakePath)
